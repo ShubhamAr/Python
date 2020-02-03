051aa6820be047c8cff68950087dc65572d7dd73
ShubhamAr <aryashubham997@gmail.com>
2020-02-03 23:01:08 +0530

    Adding example file

---
+++ Basic_example.py
@@ -0,0 +1,15 @@
+a = "Guru99!"
+b = "Software Testing"
+c = 'shubham.Arya@gmail.com'
+print ("a[5]:",a[4])
+print ("b[1:5]:",b[1:6])
+print (a+b)
+print (a*2)
+print("wakaka" not in  b)
+print(b[:7]+a)
+bc = b.replace("ware","mata")
+print(bc.upper())
+print(":".join("Python"))
+print(''.join(reversed(a)))
+d = (c.split('@'))
+print(d[1])
